204ca9d0796e698e31584b5fc3a0b9946552b875
Harshavardhan Gundeti <gundeti.harsha@gmail.com>
2025-02-04 00:00:52 -0500

    Practiced Api Parameters

--- src/main/java/com/sskgt/bend/HelloController.java
+++ src/main/java/com/sskgt/bend/HelloController.java
@@ -2,14 +2,20 @@ package com.sskgt.bend;
 
 import com.sskgt.bend.service;
 
-import java.util.Map;
+//import java.util.Map;
 import java.util.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
+
+
+
 
 
 
@@ -17,7 +23,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 @RequestMapping("/api")
 public class HelloController {
 	
-	private final service servicelayer;
+/*-----------------------------------------------Date: 02/03/25 SpringBoot API Parameters---------------------------------------*/
+
+	// Query Parameter
+
+	 @GetMapping("/queryParameter")
+	 public String queryParam(@RequestParam(name = "name", defaultValue = "Guest") String name) {
+		 return "Hello " + name;
+	 }
+	 
+	 
+	//Path Parameter (or) URI Parameter
+	 
+	 @GetMapping("/pathParameter/{name}")
+	 public String getUserByName(@PathVariable String name) {
+		 return "Hello " + name;
+	 }
+	 
+	 //Multiple Path Parameters
+	 
+	 @GetMapping("/multiPathPractice/{name}/{role}/{Id}")
+	 public String userDetails(@PathVariable String name, @PathVariable String role, @PathVariable int Id) {
+		 return "User Details are " + name + ", " + role + ", " + Id;
+	 }
+	 
+	 //RequestBody Parameter
+	 
+	 @PostMapping("/requestBodyParameter")//post by sending json
+	 public String createUser(@RequestBody Map<String, String> requestBody){
+		 return "Name is "+requestBody.get("name") + " and Phone number is " + requestBody.get("phone");
+		 
+	 }
+	 
+	 @PostMapping("/postUserName")
+	 public String postUserName(@RequestBody String name) {
+		 return name;
+	 }
+	 
+	 //Header Parameter
+	 
+	 @GetMapping("/headerParameter")
+	 public String getHeader(@RequestHeader("User-Agent") String userAgent) {
+		 return userAgent;
+	 }
+	 
+	
+	
+/* ----------------------------------------------Date: 02/02/25 SpringBoot Fundamentals--------------------------------------------
+  	
+  	
+  	
+  	private final service servicelayer;
+ 
 	
 	
 	public HelloController(service servicelayer) {
@@ -65,5 +122,5 @@ public class HelloController {
 	@GetMapping("/h")
 	public String how() {
 		return "How are you?";
-	}
+	}*/
 }
